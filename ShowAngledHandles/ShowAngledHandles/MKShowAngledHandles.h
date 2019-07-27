bc006f2bdc0c8ebedac40d3220bc594e6aba409c
Rainer Scheichelbauer <res@glyphsapp.com>
2019-07-27 10:58:06 +0200

    Py > ObjC
    commit 91fbf63fb3b4031c4c5d420f0c6bc5a13ad0a988
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 10:54:04 2019 +0200
    
        don’t strip symbols. this makes reading crash reports a bit easier.
    
    commit b3aef44a9c94bd213620b8a46a6611e350b3d3f0
    Author: Rainer Scheichelbauer <res@glyphsapp.com>
    Date:   Sat Jul 27 10:48:13 2019 +0200
    
        ignore dsstore
    
    commit f6e50e4d6c256290f1ae6cfa0ccda362d7fefdcb
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 10:47:22 2019 +0200
    
        update plugin
    
    commit 182e5ed5f3c4c3c9e3b9d8803766ce1961b6ffbe
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 10:43:05 2019 +0200
    
        fix zero handles
    
    commit d269524589083e6f958ae19600f52f9e6bd83a7e
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 10:37:49 2019 +0200
    
        apply clang format
    
    commit 1686792eadaee1f254036fd73d955f007f81e865
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 10:37:13 2019 +0200
    
        some small fixed
    
    commit 77fb517edf9942c896f09a722e4921f5d9ac783b
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 00:22:33 2019 +0200
    
        add new plugin
    
    commit 2b2abfa5114ec72e14c6ea79fdbced3d58eb952c
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 00:13:44 2019 +0200
    
        add ObjectiveC version
    
    commit 5a1dcab5341cee480f7614b637739bc19139eab0
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 00:08:14 2019 +0200
    
        optimise image
    
    commit 3e8ef3cdb4a7e6c804db3e91d6a2389398bd17cf
    Author: schriftgestalt <georg.seifert@mac.com>
    Date:   Sat Jul 27 00:08:04 2019 +0200
    
        remove old plugins

---
+++ ShowAngledHandles/ShowAngledHandles/MKShowAngledHandles.h
@@ -0,0 +1,14 @@
+//
+//  ShowAngledHandles.h
+//  ShowAngledHandles
+//
+//  Created by Georg Seifert on 26.07.19.
+//Copyright © 2019 mekkablue. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <GlyphsCore/GlyphsReporterProtocol.h>
+#import <GlyphsCore/GSGlyphViewControllerProtocol.h>
+
+@interface MKShowAngledHandles : NSObject <GlyphsReporter>
+@end
